0525ecf735bb2dadf76a5e90f297ea3c06057e40
白登超 <bacium_dc@163.com>
2025-12-01 23:59:12 +0800

    优化

--- 测试/python基础2/统计单词次数.py
+++ 测试/python基础2/统计单词次数.py
@@ -4,18 +4,20 @@
 
 str_ = "As your report on White Pollution indicates regulations on the use of plastic bags have not been implemented effectively in some areas. I am writing this letter to express my concern over the abuse of plastic bags and make some suggestions."
 
-#
-# def count(string):
-#     temp=string.split(" ")
-#     str_list=[]
-#     for word in temp:
-#         if word in str_list:
-#
-#             dict_={word:}
-#         else:
-#             num = 1
-#             dict_ = {word:num}
-#     print(temp)
-#
-# if __name__ == "__main__":
-#     count(str_)
+
+def count(string):
+    temp = string.split(" ")
+    str_list = []
+    num = 1
+    for word in temp:
+        if word in str_list:
+            num += 1
+            dict_ = {word: num}
+        else:
+            dict_ = {word: num}
+        str_list.append(dict_)
+    print(str_list)
+
+
+if __name__ == "__main__":
+    count(str_)
