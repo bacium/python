e37bf27c321674a63f8340013b0b3722beb8a329
白登超 <bacium_dc@163.com>
2025-12-21 22:30:48 +0800

    nlp分词

---
+++ 随堂/nlp/jiedba使用/jieba入门.py
@@ -0,0 +1,35 @@
+import jieba
+import os
+
+
+def dm1():
+    print(os.getcwd())
+    # 出师表
+    content = "今天下三分,益州疲敝,此诚危急存亡之秋也"
+    """
+        参数说明:
+            sentence: 文本
+            cut_all: 精确模式 True:按照词精确分词,默认Flase:按照人类正常理解进行分词;即:按所有可能出现组合进行分词
+    """
+    words = jieba.cut(sentence=content, cut_all=True)
+    # print(list(words))
+    # print(" ".join(list(words)))
+    text=jieba.lcut(sentence=content,cut_all=False)
+    # print(text)
+
+
+    # 按搜索引擎进行拆分
+    text = jieba.cut_for_search(sentence=content)
+    print(" ".join(list(text)))
+    # 繁体拆分
+    content2="煩惱即是菩提，我暫且不提"
+    text = jieba.lcut(sentence=content2)
+    print(" ".join(list(text)))
+
+
+    jieba.load_userdict("./dict/usedict.txt")
+    result = jieba.lcut(sentence=content)
+    print(f"result{result}")
+
+if __name__ == '__main__':
+    dm1()
